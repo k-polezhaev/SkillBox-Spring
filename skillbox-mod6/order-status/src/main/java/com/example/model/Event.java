d175cef082d5955adfc487dac223bfe00b7f29ae
k-polezhaev <136816376+k-polezhaev@users.noreply.github.com>
2024-12-18 02:26:32 +0500

    add mod6

---
+++ skillbox-mod6/order-status/src/main/java/com/example/model/Event.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+    private String status;
+    private Instant date;
+
+    public Event(String created, Instant now) {
+    }
+}
